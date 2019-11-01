f4467955d4d2f665d1e57b77be2e4eb40331a91a
Taka <hikijiri@throo.io>
2019-11-01 12:30:04 +0900

    main.jsの修正

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
 let counterAbi = [
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "increment",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getCounter",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "increment",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getCounter",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
 
 let counterSmartContractAddress = "0x530C96625B17cE527C2cb11d5202d289Ac651661";
 let myAccount;
 let counterContractInstance;
+let myContract;
 
-async function initApp() {
+function initApp() {
     try {
-        counterContractInstance = new web3.eth.Contract(counterAbi, counterSmartContractAddress);
-        myAccount = (await web3.eth.getAccounts())[0];
+        myContract = web3.eth.contract(counterAbi);
+        counterContractInstance = myContract.at(counterSmartContractAddress);
+        // myAccount = web3.eth.accounts[0];
+
     } catch (err) {
         console.log(err);
     }
 }
 
-window.incrementNumber = async () => {
-    try {
-        let option = {
-            from: myAccount,
-            gasPrice: "20000000000",
-            gas: "41000",
-        };
-
-        await counterContractInstance.methods.increment().send(option);
-    } catch (err) {
-        console.log(err);
-    }
+window.incrementNumber = () => {
+        counterContractInstance.increment(function(err, result) {
+            if(!err) {
+                console.log("ok", result);
+            } else {
+                console.log(err);
+            }
+        });
 };
 
-window.getNumber = async () => {
-    try {
-        let number = await counterContractInstance.methods.getCounter().call();
-        document.getElementById("number").innerText = number;
-    } catch (err) {
-        console.log(err);
-    }
+window.getNumber = () => {
+    counterContractInstance.getCounter(function (err, result) {
+        if (!err) {
+            document.getElementById("number").innerText = result;
+        } else {
+            console.log("err");
+        }
+    });
 };
 
 window.addEventListener('load', async function () {
 
-    if (typeof web3 !== 'undefined') {
+    if (typeof window.ethereum !== 'undefined' || typeof window.web3 !== "undefined") {
 
         let provider = web3.currentProvider;
         web3 = new Web3(provider);
